338b02d5468e90a5f65a6b60b79d4b962cb6a56a
Mahmoud Elkarargy <mahmoud.elkarargy@gmail.com>
2020-04-23 01:28:17 +0200

    Top and Button texts

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,15 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate {
+UINavigationControllerDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var bottomTextField: UITextField!
     
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var pickButton: UIBarButtonItem!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
 
+    @IBOutlet weak var topTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        self.topTextField.delegate = self
+        self.bottomTextField.delegate = self
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -31,7 +42,11 @@ UINavigationControllerDelegate {
         controller.sourceType = .photoLibrary
         self.present(controller, animated: true, completion: nil)
     }
-    //Add cancel fn
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
         
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
@@ -39,6 +54,15 @@ UINavigationControllerDelegate {
         }
         else{
             //Show Error msg here!
+            let controller = UIAlertController()
+            controller.title = "Error"
+            controller.message = "Please try agian"
+            
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { action in self.dismiss(animated: true, completion: nil)
+            }
+            
+            controller.addAction(okAction)
+            self.present(controller, animated: true, completion: nil)
         }
         self.dismiss(animated: true, completion: nil)
     }
@@ -50,5 +74,26 @@ UINavigationControllerDelegate {
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP"{
+            topTextField.text = ""
+        }
+        else if textField.text == "BOTTOM"{
+            bottomTextField.text = ""
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black ,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  -3.0
+    ]
+    
 }
 
